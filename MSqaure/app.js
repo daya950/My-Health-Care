7c97550b9aa774d60639fa89df8a1be28546cc56
Mohit.Kumar <daya950@gmail.com>
2016-10-07 13:14:05 +0530

    New Project

--- MSqaure/app.js
+++ MSqaure/app.js
@@ -39,7 +39,7 @@ var bodyParser = require('body-parser'),
 }*/
 
 var app = express();
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 3000));
 //app.set('view engine', 'ejs');
 //app.use(bodyParser.json({ verify: verifyRequestSignature }));
 app.use(bodyParser.json());
