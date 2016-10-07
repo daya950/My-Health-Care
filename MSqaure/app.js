8ad4496a8022872484df6d415081d565186091b1
Mohit.Kumar <daya950@gmail.com>
2016-10-07 16:33:35 +0530

    Message send to sfdc

--- MSqaure/app.js
+++ MSqaure/app.js
@@ -122,60 +122,33 @@ function sendTextMessage(recipientId, messageText) {
  * simply confirm that we've received the attachment.
  * 
  */
-function receivedMessage(event) {
+function receivedMessage(event) { 
 	var senderID = event.sender.id;
 	var recipientID = event.recipient.id;
 	var timeOfMessage = event.timestamp;
 	var message = event.message;
+	var data = {text : message, recid : recipientID};
+	request({
+		uri : 'https://msquare-developer-edition.ap2.force.com/services/apexrest/sfdcwebhook',
+		qs : {
+			access_token : PAGE_ACCESS_TOKEN
+		},
+		method : 'POST',
+		json : data
+	}, function(error, response, body) {
+		if (!error && response.statusCode === 200) {
+			var recipientId = body.recipient_id;
+			var messageId = body.message_id;
 
-	console.log("Received message for user %d and page %d at %d with message:",	senderID, recipientID, timeOfMessage);
-	console.log(JSON.stringify(message));
-
-	var isEcho = message.is_echo;
-	var messageId = message.mid;
-	var appId = message.app_id;
-	var metadata = message.metadata;
-
-	// You may get a text or attachment but not both
-	var messageText = message.text;
-	var messageAttachments = message.attachments;
-	var quickReply = message.quick_reply;
-
-	if (isEcho) {
-		console.log("Received echo for message %s and app %d with metadata %s", messageId, appId, metadata);
-		return;
-	} else if (quickReply) {
-		var quickReplyPayload = quickReply.payload;
-		console.log("Quick reply for message %s with payload %s", messageId, quickReplyPayload);
-		sendTextMessage(senderID, "Quick reply tapped");
-		return;
-	}
-
-	if (messageText) {
-		// If we receive a text message, check to see if it matches any special
-		// keywords and send back the corresponding example. Otherwise, just
-		// echo
-		// the text we received.
-		/*switch (messageText) {
-			case 'image': sendImageMessage(senderID); break;
-			case 'gif': sendGifMessage(senderID); break;
-			case 'audio': sendAudioMessage(senderID); break;
-			case 'video': sendVideoMessage(senderID); break;
-			case 'file': sendFileMessage(senderID);	break;
-			case 'button': sendButtonMessage(senderID); break;
-			case 'generic': sendGenericMessage(senderID); break;
-			case 'receipt': sendReceiptMessage(senderID); break;
-			case 'quick reply': sendQuickReply(senderID); break;
-			case 'read receipt': sendReadReceipt(senderID); break;
-			case 'typing on': sendTypingOn(senderID); break;
-			case 'typing off': sendTypingOff(senderID); break;
-			case 'account linking': sendAccountLinking(senderID); break;
-			default: sendTextMessage(senderID, messageText);
-		}*/
-		sendTextMessage(senderID, messageText);
-	} else if (messageAttachments) {
-		sendTextMessage(senderID, "Message with attachment received");
-	}
+			if (messageId) {
+				console.log("Successfully sent message with id %s to Salesforce Agent",	messageId, recipientId);
+			} else {
+				console.log("Something Wrong in receivedMessage Function");
+			}
+		} else {
+			console.error("Error Occured in receivedMessage Function ", response.statusCode, response.statusMessage, body.error);
+		}
+	});
 }
 
 /*
