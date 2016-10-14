ac11e97a694349f031c40a115c48c9c0a6a7c966
Mohit.Kumar <daya950@gmail.com>
2016-10-14 17:22:43 +0530

    33

--- MSqaure/app.js
+++ MSqaure/app.js
@@ -136,7 +136,7 @@ function getMessageForFb(key, id, token, recipient, sequenceNum) {
 		}
 	});
 	if (typeof sequenceNum === "number") {
-		setTimeout( function() {getMessageForFb(key, id, token, recipient, sequenceNum);}, 500);		
+		setTimeout( function() {getMessageForFb(key, id, token, recipient, sequenceNum);}, 10000);		
 	}
 }
 
